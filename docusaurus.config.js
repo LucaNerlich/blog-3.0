0b5913ab8cd662d5248bc20cb70336c3c1c343e4
Luca Nerlich <luca.nerlich@googlemail.com>
2021-12-18 17:04:45 +0100

    Add imprint.md update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,117 +6,117 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Luca Nerlich - Tech Documentation',
-  tagline: 'A Blog ~ ish',
-  url: 'https://blog.lucanerlich.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'LucaNerlich',
-  projectName: 'blog-3.0',
+    title: 'Luca Nerlich - Tech Documentation',
+    tagline: 'A Blog ~ ish',
+    url: 'https://blog.lucanerlich.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'LucaNerlich',
+    projectName: 'blog-3.0',
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/LucaNerlich/blog-3.0',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/LucaNerlich/blog-3.0',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/LucaNerlich/blog-3.0',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/LucaNerlich/blog-3.0',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Luca Nerlich',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {
-            to: '/docs/aem/intro',
-            label: 'AEM',
-            position: 'left'
-          },
-          {
-            href: 'https://github.com/LucaNerlich/blog-3.0',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Documentation',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          {
-            title: 'More',
-            items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/LucaNerlich/blog-3.0',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Luca Nerlich - Built with Docusaurus v2.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                title: 'Luca Nerlich',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Documentation',
+                    },
+                    {
+                        to: '/docs/aem/intro',
+                        label: 'AEM',
+                        position: 'left'
+                    },
+                    {
+                        href: 'https://github.com/LucaNerlich/blog-3.0',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Documentation',
+                                to: '/docs/intro',
+                            },
+                            {
+                                label: 'Imprint',
+                                to: '/imprint',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Socials',
+                        items: [
+                            {
+                                label: 'Xing',
+                                href: 'https://www.xing.com/profile/Luca_Nerlich',
+                            },
+                            {
+                                label: 'LinkedIn',
+                                href: 'https://www.linkedin.com/in/lucanerlich/',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Tech',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/LucaNerlich/blog-3.0',
+                            },
+                            {
+                                label: 'Gitlab',
+                                href: 'https://gitlab.com/lucanerlich',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Luca Nerlich - Built with Docusaurus v2.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
---
+++ src/pages/imprint.md
@@ -0,0 +1,19 @@
+# Impressum
+
+Nachstehende Informationen erteilen wir gemäß § 5 Telemediengesetz (TMG):
+
+Diese Webseite wird vertreten durch die Person
+
+```text
+Luca Nerlich
+Wandsbeker Chaussee 114
+22089 Hamburg
+Germany
+```
+
+
+Dispute Resolution Information
+
+Die EU-Kommission bietet die Möglichkeit zur Online-Streitbeilegung auf einer von ihr betriebenen Online-Plattform. Diese Plattform ist über den externen Link http://ec.europa.eu/consumers/odr/ zu erreichen. Zu einer Teilnahme an einem Schlichtungsverfahren sind wir nicht verpflichtet und können die Teilnahme an einem solchen Verfahren leider auch nicht anbieten.
+
+The European Commission offers an Online Dispute Resolution (ODR) Platform at http://ec.europa.eu/consumers/odr/. However, we are neither obliged nor willing to participate in a dispute resolution procedure before a consumer mediation entity.
