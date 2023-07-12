96e83932f02e3bfa13681a64103352d9981c5c05
Luca Nerlich <luca.nerlich@googlemail.com>
2023-07-12 08:42:43 +0200

    add mastodon verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         <Layout
             title={`Hello from ${siteConfig.title}`}
             description="Description will go into a meta tag in <head />">
-            <a rel="me" href="https://mastodon.social/@luca00">Mastodon</a>
+            <a style={{display: 'none'}} rel="me" href="https://mastodon.social/@luca00">Mastodon</a>
             <HomepageHeader/>
             <main>
                 <HomepageFeatures/>
