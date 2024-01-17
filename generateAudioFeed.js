2ca7f973fc41651d717f278d549bbae857277468
Luca Nerlich <luca.nerlich@gmail.com>
2024-01-17 08:35:30 +0100

    prepare rss feed gen

---
+++ generateAudioFeed.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const xml2js = require('xml2js');
+const crypto = require('crypto');
+const https = require('https');
+const {URL} = require('url');
+
+const basepath = './static/audiofeed';
+
+function convertToPubDateFormat(dateString) {
+    const date = new Date(dateString);
+    const options = {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZone: 'UTC'
+    };
+    return date.toLocaleDateString('en-GB', options) + ' +0000';
+}
+
+function toHash(string) {
+    const hash = crypto.createHash('sha256');
+    hash.update(string);
+    return hash.digest('hex');
+}
+
+async function yamlObjectToXml(yamlObject) {
+    const url = new URL(yamlObject.url);
+    const options = {
+        method: 'HEAD',
+        host: url.hostname,
+        path: url.pathname,
+    };
+
+    const getFileSize = new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            console.log('status', res.statusCode + ' ' + options.host + options.path);
+            resolve(res.headers['content-length']);
+        });
+        req.on('error', reject);
+        req.end();
+    });
+
+    const fileSize = await getFileSize;
+
+    return {
+        'title': yamlObject.title,
+        'pubDate': convertToPubDateFormat(yamlObject.date),
+        'guid': {
+            _: toHash(yamlObject.url),
+            $: {isPermaLink: 'false'},
+        },
+        'itunes:image': {
+            $: {
+                href: yamlObject.image,
+            },
+        },
+        'description': yamlObject.description,
+        'author': 'm10z@posteo.de',
+        'itunes:duration': yamlObject.seconds,
+        'link': 'https://m10z.de',
+        'enclosure': {
+            $: {
+                url: yamlObject.url,
+                length: fileSize,
+                type: 'audio/mpeg',
+            },
+        },
+    };
+}
+
+async function insertItemsToXMLFile(xmlFilePath, yamlObjects) {
+    const data = fs.readFileSync(xmlFilePath);
+
+    xml2js.parseString(data, async (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        result.rss.channel[0].item = await Promise.all(yamlObjects.map(yamlObjectToXml));
+
+        const builder = new xml2js.Builder({
+            renderOpts: {'pretty': true, 'indent': '    ', 'newline': '\n'},
+            cdata: true
+        });
+        const xml = builder.buildObject(result);
+
+        fs.writeFileSync(basepath + '.xml', xml);
+    });
+}
+
+console.log('Creating audiofeed.xml');
+const yamlData = fs.readFileSync(basepath + '.yaml', 'utf8');
+const yamlObjects = yaml.load(yamlData);
+insertItemsToXMLFile('./templates/rss-channel.xml', yamlObjects)
+    .then(success => console.log('Successfully created audiofeed.xml'))
+    .catch(error => console.error('Failed to generate audiofeed.xml', error));
---
+++ parseToYaml.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+const jsYaml = require('js-yaml');
+
+const basepath = './static/sfs';
+
+fs.readFile(basepath + '.xml', 'utf8', function (err, data) {
+    if (err) {
+        return console.log('Failed to read file: ' + err);
+    }
+    xml2js.parseString(data, function (err, result) {
+        if (err) {
+            return console.log('Failed to parse XML: ' + err);
+        }
+
+        let items = result.rss.channel[0].item;
+
+        // clean up items
+        items = items?.map(item => {
+            let cleanedItem = {};
+            for (let key in item) {
+                switch (key) {
+                    case 'itunes:image':
+                        cleanedItem['image'] = item[key][0]['$']['href'];
+                        break;
+                    case 'enclosure':
+                        cleanedItem['url'] = item[key][0]['$']['url'];
+                        break;
+                    case 'guid':
+                    case 'author':
+                        // ignore
+                        break;
+                    case 'itunes:duration':
+                        cleanedItem['seconds'] = convertToSeconds(item[key][0]);
+                        break;
+                    case 'pubDate':
+                        cleanedItem['date'] = convertToDate(item[key]);
+                        break;
+                    case 'description':
+                        cleanedItem[key] = item[key][0].replace(/(\r\n|\r|\n)/g, '\n');
+                        break;
+                    default:
+                        cleanedItem[key] = item[key][0];
+                }
+            }
+            return cleanedItem;
+        });
+
+        let yamlStr = jsYaml.dump(items, {lineWidth: -1});
+        fs.writeFile(basepath + '.yaml', yamlStr, 'utf8', function (err) {
+            if (err) {
+                return console.log('Failed to create YAML file: ' + err);
+            }
+            console.log('Converted XML to YAML successfully!');
+        });
+    });
+});
+
+function convertToDate(pubDateString) {
+    const date = new Date(pubDateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+    const day = ('0' + date.getUTCDate()).slice(-2);
+    const hour = ('0' + date.getUTCHours()).slice(-2);
+    const minute = ('0' + date.getUTCMinutes()).slice(-2);
+
+    return `${year}-${month}-${day}T${hour}:${minute}`;
+}
+
+function convertToSeconds(time) {
+    if (time.includes(':')) {
+        const splitTime = time.split(':');
+        const hours = +splitTime[0];
+        const minutes = +splitTime[1];
+        const seconds = +splitTime[2];
+        return hours * 3600 + minutes * 60 + seconds;
+    } else if (isNaN(time)) { // Is not a number
+        console.error('Invalid input');
+        return 0;
+    } else {
+        return +time;
+    }
+}
